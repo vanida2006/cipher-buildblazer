8a63626bdf74769aa8cb9949ef544eab31bce31f
2003vanessa <vanessa.quadras2003@gmail.com>
2026-09-25 01:09:32 +0530

    Fix Render dynamic port

--- server/index.js
+++ server/index.js
@@ -44,6 +44,6 @@ app.use((err, _req, res, _next) => {
   res.status(500).json({ error: 'Something went wrong' });
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, '0.0.0.0', () => console.log(`CIPHER running on http://localhost:${port} (or http://127.0.0.1:${port})`));
 
